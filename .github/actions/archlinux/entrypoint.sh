e39944d15fd0326e8740045750dc9a27fb84120d
James C Kimble <me@jckimble.com>
2022-07-20 01:56:25 -0500

    fix: sudo find

--- .github/actions/archlinux/entrypoint.sh
+++ .github/actions/archlinux/entrypoint.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-listDir(){
-    echo $1
-    ls -lAh $1
-}
 set -ex
 pacman-key --init
-pacman -Syu --noconfirm --noprogressbar --ignore linux --ignore linux-firmwre --needed base-devel devtools dbus sudo python-pip
-
-#pip install -q pacrepo-cli
-#pacrepo-cli add --siglevel "Optional TrustAll" jckimble https://github.com/jckimble/pkgbuilds/releases/download/repository
-#pip uninstall -q --yes pacrepo-cli
-#pacman -Sy
+pacman -Syu --noconfirm --noprogressbar --ignore linux --ignore linux-firmwre --needed base-devel devtools dbus sudo
 
 dbus-uuidgen --ensure=/etc/machine-id
 
@@ -22,7 +13,15 @@ echo "%wheel ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/wheel
 og=$(stat -c '%u:%g' .)
 od=$(pwd)
 chown -R build: .
-cd $1
-sudo -u build --preserve-env=PACKAGER $2
-cd "$od"
+if [ "$2" == "exec" ]; then
+    cd $1
+    sudo -u build --preserve-env=PACKAGER $3
+    cd "$od"
+elif [ "$2" == "makepkg" ]; then
+    cd $1
+    sudo -u build --preserve-env=PACKAGER makepkg -sr -C -c --noconfirm --noprogressbar
+    cd "$od"
+elif [ "$2" == "repo-add" ]; then
+    find $1 -iname *.pkg.tar.zst -exec sudo -u build --preserve-env=PACKAGER repo-add -R -p -q ${3}.db.tar.xz {} \;
+fi
 chown -R "$og" .
