b9a03c52fef83381a51cd6f098520113294de073
James C Kimble <me@jckimble.com>
2022-12-13 09:45:59 -0600

    feat: startv2

--- .github/actions/archlinux/entrypoint.sh
+++ .github/actions/archlinux/entrypoint.sh
@@ -6,15 +6,16 @@ pacman -Syuq --noconfirm --noprogressbar --ignore linux --ignore linux-firmware 
 og=$(stat -c '%u:%g' .)
 od=$(pwd)
 chown -R build: .
-if [ "$2" == "exec" ]; then
-    cd $1
-    sudo -u build --preserve-env=PACKAGER $3
-    cd "$od"
-elif [ "$2" == "makepkg" ]; then
-    cd $1
-    sudo -u build --preserve-env=PACKAGER makepkg -sr -C -c --noconfirm --noprogressbar
-    cd "$od"
-elif [ "$2" == "repo-add" ]; then
-    find $1 -iname \*.pkg.tar.zst -exec sudo -u build --preserve-env=PACKAGER repo-add -R -p -q ${3}.db.tar.xz {} \;
+
+if [ ! -z "${SECRET}" ]; then
+openssl aes-256-cbc -d -a -pbkdf2 -in ${GPGKEY} -pass pass:${SECRET} | sudo -u build gpg --import
+unset GPGKEY
+unset SECRET
 fi
+
+cd $1
+shift
+sudo -u build --preserve-env=PACKAGER $@
+
+cd "$od"
 chown -R "$og" .
